894ee8c8eb4e9bfbdb564e182e361e741056130b
hobbyluv <princewillchime43@gmail.com>
2021-01-12 07:29:23 +0100

    added project context and landing page route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import DashboardRoute from "./Routes/DashboardRoute";
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <DashboardRoute path="/" component={LandingPage} exact />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Context_file/DashBoardContext.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const DashBoardContext = React.createContext([{}, () => {}]);
+
+const DashBoardProvider = (props) => {
+  const [Users, setUsers] = useState({
+    usersList: [],
+  });
+
+  return (
+    <DashBoardContext.Provider
+      //assign the provider values
+      value={[Users, setUsers]}
+    >
+      {props.children}
+    </DashBoardContext.Provider>
+  );
+};
+
+export { DashBoardContext, DashBoardProvider };
