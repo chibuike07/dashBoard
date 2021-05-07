d92215ed0a7b5d76b2e111fa354e90eacdcae587
hobbyluv <princewillchime43@gmail.com>
2021-05-07 22:56:03 +0100

    handled the search of user from the input left searchbar

--- src/Components/LandingPageLeftView/UserSearchBar.jsx
+++ src/Components/LandingPageLeftView/UserSearchBar.jsx
@@ -1,14 +1,35 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import Input from "../../Common/Input.component/Input";
 import UserInputStyles from "../../Styles/LandingpageLeftView/UserInput.module.css";
+import { DashBoardContext } from "../../Context_file/DashBoardContext";
 
 const UserSearchBar = () => {
   const { container, input, icon } = UserInputStyles;
   const [findUser, setfindUser] = useState("");
+  const [, setState] = useContext(DashBoardContext);
+
+  useEffect(() => {
+    const handleSearchGender = () => {
+      if (findUser === "") {
+        setState((data) => ({ ...data, tags: "all users" }));
+      } else {
+        setState((data) => ({
+          ...data,
+          gender: findUser.toLocaleLowerCase(),
+          tags: `${findUser} users`,
+        }));
+      }
+    };
+    handleSearchGender();
+
+    return [handleSearchGender];
+  }, [setState, findUser]);
+
   return (
     <div className={container}>
+      <h2>hello</h2>
       <Input
         icon={
           <FontAwesomeIcon
@@ -18,7 +39,7 @@ const UserSearchBar = () => {
             opacity={findUser ? 0 : 1}
           />
         }
-        placeholder="Find a user"
+        placeholder="Find a user eg: male, female and all"
         name="user input"
         value={findUser}
         className={input}
